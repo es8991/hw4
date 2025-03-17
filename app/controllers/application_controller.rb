820837e59bece74c66e15ff0bd3162fb057fd037
Sarah El-Guindy <sarah.elguindy@kellogg.northwestern.edu>
2025-03-17 04:02:06 +0000

    boostrap code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
-  before_action :current_user
+  before_action :set_current_user
 
-  def current_user
-    @current_user = User.find_by({ "id" => session["user_id"] })
+  def set_current_user
+    @current_user = User.find_by(id: session[:user_id])
   end
+
+  def require_login
+    unless @current_user
+      flash[:alert] = "Please login to view this page."
+      redirect_to "/login"
+    end
+  end
+
 end
